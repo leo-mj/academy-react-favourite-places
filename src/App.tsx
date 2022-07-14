1868b5ea561c9e51398d73e8524180223f9b1f36
Leo <leo.maedje@gmail.com>
2022-07-14 15:16:13 +0100

    Ran prettier

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import AppHeader from "./components/AppHeader";
 import MainContent from "./components/MainContent";
 import AppFooter from "./components/AppFooter";
 
-
 function App(): JSX.Element {
   return (
-  <>
-    <AppHeader/>
-    <MainContent/>
-    <AppFooter/>
-  </>
-  )}
+    <>
+      <AppHeader />
+      <MainContent />
+      <AppFooter />
+    </>
+  );
+}
 
 export default App;
--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,7 +1,5 @@
 function AppHeader(): JSX.Element {
-    return (
-        <h1>Leo's Favourite Places</h1>
-    )
+  return <h1>Leo's Favourite Places</h1>;
 }
 
 export default AppHeader;
